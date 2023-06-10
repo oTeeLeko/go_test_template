8526983272e7933bb64a607025c907dbf836e2d3
Supakan Sriwichai <leksupakan@gmail.com>
2023-06-10 14:49:17 +0700

    Update func

--- starter.go
+++ starter.go
@@ -7,3 +7,11 @@ import (
 func SayHello(name string) string {
 	return fmt.Sprintf("Hello %v. Welcome!", name)
 }
+
+func evenOrOdd(num int) string {
+	evenOrOdd := "odd"
+	if num%2 == 0 {
+		evenOrOdd = "even"
+	}
+	return fmt.Sprintf("%v is an %v number", num, evenOrOdd)
+}
--- starter_test.go
+++ starter_test.go
@@ -13,3 +13,25 @@ func TestSayHello(t *testing.T) {
 	another_greeting := SayHello("Fern")
 	assert.Equal(t, "Hello Fern. Welcome!", another_greeting)
 }
+
+func TestEvenOrOdd(t *testing.T) {
+	t.Run("Test Non Negative Even Number", func(t *testing.T) {
+		assert.Equal(t, "50 is an even number", evenOrOdd(50))
+		assert.Equal(t, "42 is an even number", evenOrOdd(42))
+		assert.Equal(t, "0 is an even number", evenOrOdd(0))
+	})
+	t.Run("Test Negative Even Number", func(t*testing.T) {
+		assert.Equal(t, "-50 is an even number", evenOrOdd(-50))
+		assert.Equal(t, "-42 is an even number", evenOrOdd(-42))
+	})
+	t.Run("Test Non Negative Odd Number", func(t*testing.T) {
+		assert.Equal(t, "3 is an odd number", evenOrOdd(3))
+		assert.Equal(t, "9 is an odd number", evenOrOdd(9))
+		assert.Equal(t, "55 is an odd number", evenOrOdd(55))
+	})
+	t.Run("Test Negative Odd Number", func(t*testing.T) {
+		assert.Equal(t, "-3 is an odd number", evenOrOdd(-3))
+		assert.Equal(t, "-9 is an odd number", evenOrOdd(-9))
+		assert.Equal(t, "-55 is an odd number", evenOrOdd(-55))
+	})
+}
